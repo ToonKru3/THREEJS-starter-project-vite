6d4e8764bcfae7f6ae6a2ffb2c816f1f1593d554
ToonKrub <supadett.pins@gmail.com>
2022-10-16 16:37:37 +0700

    adding resize adding pixelratio

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-let renderer , canvasWebGl , scene , camera;
+let renderer , canvasWebGl , scene , camera , control;
 
 const sizes = {
   width: window.innerWidth,
@@ -13,17 +14,35 @@ function init()
   canvasWebGl = document.querySelector('canvas.webgl');
   scene = new THREE.Scene()
 
-
   renderer = new THREE.WebGLRenderer({
     canvas: canvasWebGl,
     antialias: true
   })
   renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 
   camera = new THREE.PerspectiveCamera(45 , sizes.width / sizes.height , 0.1 , 3000 )
   camera.position.z = 3
   scene.add(camera)
+
+  control = new OrbitControls(camera, canvasWebGl)
+  control.enableDamping = true
+
+  window.addEventListener('resize', () =>
+  { 
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width , sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+
+
+
 
   const box = new THREE.BoxGeometry(1,1,1)
   const material =new THREE.MeshBasicMaterial({color: 'red'})
@@ -33,7 +52,6 @@ function init()
 
 
 }
-
 init()
 
 
@@ -43,8 +61,8 @@ function animate()
   requestAnimationFrame(animate)
 
 
-  // controls.update()
   // stats.update()
+  control.update()
   renderer.render( scene, camera );
 }
 
