2bf65214d6572f1b57690de68c7755d56e88beb1
ToonKrub <supadett.pins@gmail.com>
2022-10-16 17:03:41 +0700

    adding vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
+
 
 export default defineConfig({
-    root: './',
-    publicDir: '../public',
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    },
     server: {
-        watch: {
-            usePolling: true     // Change it's to true 
-        }
-    }
-})
+      watch: {
+        usePolling: true
+      }
+    },
+  })
